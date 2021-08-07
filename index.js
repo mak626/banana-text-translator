cdd0f28f624404a7ac0a2f6c527fa8644b6974d5
M Aswin Kishore <mak26.work@gmail.com>
2021-08-08 00:20:56 +0530

    Refactor index.js

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const API_URL = "https://api.funtranslations.com/translate/minion.json?text=CONT
 submitButton.addEventListener("click", () => {
     const givenData = document.querySelector("#input-field").value;
     const finalUrl = encodeURI(API_URL.replace("CONTENT", givenData));
-    console.log(givenData);
-
     fetch(finalUrl)
         .then(res => res.json())
         .then(data => outputTextField.innerText = data.contents.translated)
-        .catch(e => alert(`Error Occured: ${e.message}`));
+        .catch(e => alert('Error Occured'));
 });
